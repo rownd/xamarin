bc6e440b68f3298f666055b5c57bc66e2496662c
Matt Hamann <mhamann@rownd.io>
2023-06-21 12:32:21 -0400

    chore: cleanup

--- RowndXamarinExample/RowndXamarinExample/App.xaml.cs
+++ RowndXamarinExample/RowndXamarinExample/App.xaml.cs
@@ -6,18 +6,12 @@ namespace RowndXamarinExample
 {
     public partial class App : Application
     {
-        public RowndInstance Rownd;
+        public RowndInstance Rownd { get; private set; }
 
         public App()
         {
-            var rowndConfig = new Rownd.Xamarin.Core.Config();
-            rowndConfig.ApiUrl = "https://api.us-east-2.dev.rownd.io";
-            rowndConfig.HubUrl = "https://hub.dev.rownd.io";
-            rowndConfig.SubdomainExtension = ".dev.rownd.link";
-            rowndConfig.Customizations.SheetBackgroundColor = Color.FromHex("#1f2550");
-
-            Rownd = RowndInstance.GetInstance(this, rowndConfig);
-            Rownd.Configure("b60bc454-c45f-47a2-8f8a-12b2062f5a77");
+            Rownd = RowndInstance.GetInstance(this);
+            Rownd.Configure("YOUR_APP_KEY");
 
             DependencyService.RegisterSingleton<IRowndInstance>(Rownd);
 
