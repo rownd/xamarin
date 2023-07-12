6bc861db51f4ccae63a113d9d527caf7bfd2ce01
Matt Hamann <mhamann@rownd.io>
2023-07-12 01:25:08 -0400

    fix(sheet): soft keyboard detect ios

--- Rownd.iOS/Hub/HubWebViewRenderer.cs
+++ Rownd.iOS/Hub/HubWebViewRenderer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using Foundation;
 using Rownd.Xamarin.Core;
 using Rownd.Xamarin.Hub;
 using Rownd.Xamarin.iOS.Hub;
+using UIKit;
 using WebKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -12,10 +14,15 @@ namespace Rownd.Xamarin.iOS.Hub
     public class HubWebViewRenderer : WkWebViewRenderer, IWKScriptMessageHandler
     {
         private WKUserContentController userController;
+        private NSObject notification;
 
         public HubWebViewRenderer() : this(new WKWebViewConfiguration())
         {
             CustomUserAgent = Constants.DEFAULT_WEB_USER_AGENT;
+            notification = UIKeyboard.Notifications.ObserveDidShow((sender, args) =>
+            {
+                ((HubWebView)Element).HandleKeyboardStateChange(true);
+            });
         }
 
         public HubWebViewRenderer(WKWebViewConfiguration config) : base(config)
